e1c4cb2b87106757c9a0b674eabede6674996626
victorqarnot <victor.joly@qarnot-computing.com>
2019-12-17 10:28:41 +0100

    Hello World Example n°1

--- FreeFem_Days/1_hello_world.py
+++ FreeFem_Days/1_hello_world.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import qarnot
-conn = qarnot.connection.Connection(client_token="xxxx_mytoken_xxxx")
+conn = qarnot.connection.Connection(client_token="<<<XXX put your token here XXX>>>")
 task = conn.create_task('helloworld', 'docker-batch', 1)
 task.constants['DOCKER_CMD'] = 'echo hello world!'
 task.run()
